9b02e52238cd4601000c79ab17cbd50bc73e4278
Mathias Fruergaard Holst <MFHo.skp>
2020-08-03 13:03:43 +0200

    +Projekt(er) oprettet +Classes +Oprettet 2 objekter

---
+++ PersonRegister/PersonRegister/PersonRegister/person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PersonRegister
+{
+    public class person
+    {
+       public string name;
+       public int age;
+       public Job job;
+
+       public person(string name, int age, Job job)
+       {
+            this.name = name;
+            this.age = age;
+            this.job = job;
+       }
+    }
+    public class Job
+    {
+        public string jobtitle;
+        public string orgName;
+        public int salary;
+
+        public Job(string jobtitle, string orgName, int salary)
+        {
+            this.jobtitle = jobtitle;
+            this.orgName = orgName;
+            this.salary = salary;
+        }
+    }
+}
---
+++ PersonRegister/PersonRegister/PersonRegisterTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PersonRegister;
+
+namespace PersonRegisterTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Job job1 = new Job("Astronaut", "SpaceX", 100000);
+            person person1 = new person("Timmy", 33, job1);
+
+            person1.job.salary = 999999;
+
+            Console.WriteLine(person1.name);
+            Console.WriteLine(person1.age);
+            Console.WriteLine(person1.job.jobtitle);
+            Console.WriteLine(person1.job.salary);
+        }
+    }
+}
