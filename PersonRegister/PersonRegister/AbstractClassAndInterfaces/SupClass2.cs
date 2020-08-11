2f2a0daab6c1f16d65bb5b46300046c6e9a07d05
Mathias Fruergaard Holst <MFHo.skp>
2020-08-11 14:34:51 +0200

    +AbstractClass +AbstractFunction +Interface

---
+++ PersonRegister/PersonRegister/AbstractClassAndInterfaces/SupClass2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClassAndInterfaces
+{
+    public class SupClass2 : AbstractClass, IMathInterface
+    {
+        int IMathInterface.plus2()
+        {
+            return 10 + 2;
+        }
+
+        int IMathInterface.gange2()
+        {
+            return 10 * 2;
+        }
+
+        int IMathInterface.minus2()
+        {
+            return 10 - 2;
+        }
+        public override int AbstractFunc()
+        {
+            return 2;
+        }
+    }
+}
---
+++ PersonRegister/PersonRegister/AbstractClassAndInterfaces/SupClass3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClassAndInterfaces
+{
+    public class SupClass3 : AbstractClass
+    {
+        public override int AbstractFunc()
+        {
+            return 3;
+        }
+    }
+}
