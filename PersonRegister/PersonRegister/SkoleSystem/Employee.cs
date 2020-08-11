2050c68744616ad55b540659066900b90aa182ef
Mathias Fruergaard Holst <MFHo.skp>
2020-08-11 08:41:32 +0200

    +Nedarving i skolesystemet

--- PersonRegister/PersonRegister/SkoleSystem/Employee.cs
+++ PersonRegister/PersonRegister/SkoleSystem/Employee.cs
@@ -4,13 +4,10 @@ using System.Text;
 
 namespace SkoleSystem
 {
-    public class Employee
+    public class Employee : Person
     {
-        public string name;
-        public int age;
         public bool salaryPayed;
-        public Course course;
-
+        
         public Employee(string name, int age, bool salaryPayed, Course course)
         {
             this.name = name;
--- PersonRegister/PersonRegister/SkoleSystem/Student.cs
+++ PersonRegister/PersonRegister/SkoleSystem/Student.cs
@@ -4,12 +4,9 @@ using System.Text;
 
 namespace SkoleSystem
 {
-    public class Student
+    public class Student : Person
     {
-        public string name;
-        public int age;
         public Employee teacher;
-        public Course course;
         public Student(string name, int age, Employee teacher, Course course)
         {
             this.name = name;
