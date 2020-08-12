f169c89c242020dab3794ca04dd6f971ee01708b
Mathias Fruergaard Holst <MFHo.skp>
2020-08-12 12:28:52 +0200

    +Multithreading gæt et tal mellem 1-10 +Timer

---
+++ PersonRegister/PersonRegister/Multithreading/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    public class Timer
+    {
+        private static CancellationTokenSource tokenSource = new CancellationTokenSource();
+        public static Task GuessANumber(int seconds, int repeats)
+        {
+            CancellationToken token = tokenSource.Token;
+            Task task = new Task(() =>
+            {
+                for(int i = 0; i < repeats && !token.IsCancellationRequested; i++)
+                {
+                    Console.WriteLine($"You have {seconds} seconds left!");
+                    seconds -= 1;
+                    if(!token.IsCancellationRequested) Task.Delay(1000).Wait();
+                }
+            });
+            task.Start();
+
+
+
+            return task;
+        }
+
+        public static void StopTasks()
+        {
+            tokenSource.Cancel();
+        }
+    }
+}
